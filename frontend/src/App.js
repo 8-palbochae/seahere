b132ece71732d8aa1f473f52f11e74b1d1b53ee5
woneveryady <woneveryday@naver.com>
2024-07-19 15:59:59 +0900

    Design : period

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import "./App.css";
+import Period from "./components/Trade/Period";
 
 function App() {
 	return (
-		<div className="flex justify-center items-center min-h-screen bg-gray-100">
+		<div className="flex justify-center items-center min-h-screen to-blue-600">
 			<div className="w-full max-w-4xl bg-white p-8 shadow-md">
-				<h1 className="text-2xl font-bold mb-4">Hello, seahere!</h1>
-				<p className="text-gray-700">This is your main content area.</p>
+				<Period/>
 			</div>
 		</div>
 	);
