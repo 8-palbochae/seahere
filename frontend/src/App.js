ff0ac36e54c13168bfae40980481adaf12802021
kimdodo97 <eh4536@naver.com>
2024-07-19 13:06:08 +0900

    Init : 프론트엔드 폰트 설정

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,9 @@ function App() {
 			<div className="w-full max-w-4xl bg-white p-8 shadow-md">
 				<h1 className="text-2xl font-bold mb-4">Hello, seahere!</h1>
 				<p className="text-gray-700">This is your main content area.</p>
+				<p className="">안녕하세요 여보소 입니다</p>
+				<p className="text-gray-700">안녕하세요 여보소 입니다</p>
+				<button>버튼</button>
 			</div>
 		</div>
 	);
