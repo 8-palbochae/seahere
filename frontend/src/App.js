4a33bd60b974ad915fc0bb1a2bb092edd994317d
Jeong gi seok <wjdrltjr5@naver.com>
2024-07-19 15:11:58 +0900

    Design : header, main 기본 위치

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,20 @@
 import React from "react";
 import "./App.css";
+import { Header } from "./compoents/common/Header";
 
 function App() {
 	return (
-		<div className="flex justify-center items-center min-h-screen bg-gray-100">
-			<div className="w-full max-w-4xl bg-white p-8 shadow-md">
-				<h1 className="text-2xl font-bold mb-4">Hello, seahere!</h1>
-				<p className="text-gray-700">This is your main content area.</p>
-				<p className="">안녕하세요 여보소 입니다</p>
-				<p className="text-gray-700">안녕하세요 여보소 입니다</p>
-				<button>버튼</button>
+		<div className="flex justify-center items-center bg-gray-100">
+			<div className="w-full max-w-4xl bg-white p-8 shadow-md p-0  min-h-screen">
+				<div>
+					<Header />
+				</div>
+				<div>
+					<h1 className="text-2xl font-bold mb-4">Hello, Musinsa!</h1>
+					<p className="text-gray-700">
+						This is your main content area.
+					</p>
+				</div>
 			</div>
 		</div>
 	);
---
+++ frontend/src/compoents/common/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+// import headerIcon from "../../consts/header/header.image";
+import { ReactComponent as Bell } from "../../assets/header/bell.svg";
+
+export const Header = () => {
+	return (
+		<div>
+			<div className="flex justify-between bg-white border-b-2 border-black">
+				<div
+					className="flex-auto"
+					style={{ width: 24, height: 24 }}
+				></div>
+				<div className="flex-auto justify-items-center">헤더입니다</div>
+				<div>
+					<Bell />
+				</div>
+			</div>
+		</div>
+	);
+};
