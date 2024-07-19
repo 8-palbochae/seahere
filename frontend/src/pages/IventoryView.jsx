2be858c9716408af962993f25e01e9076cb75651
kimdodo97 <eh4536@naver.com>
2024-07-20 00:40:30 +0900

    Design : 검색어 입력 공통 컴포넌트 디자인

---
+++ frontend/src/pages/IventoryView.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import SearchInput from '../components/common/SearchInput';
+
+const IventoryView = () => {
+    return (
+        <div>
+            <SearchInput/>
+        </div>
+    );
+};
+
+export default IventoryView;
