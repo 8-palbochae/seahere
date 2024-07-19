cd3be09076522a32a45622ab01d1ea403a008248
kimdodo97 <eh4536@naver.com>
2024-07-19 15:17:24 +0900

    Design : Footer 디자인 추가

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,14 @@
 import React from "react";
 import "./App.css";
+import Bottom from './components/common/bottom/Bottom';
 
 function App() {
 	return (
-		<div className="flex justify-center items-center min-h-screen bg-gray-100">
-			<div className="w-full max-w-4xl bg-white p-8 shadow-md">
-				<h1 className="text-2xl font-bold mb-4">Hello, seahere!</h1>
-				<p className="text-gray-700">This is your main content area.</p>
-				<p className="">안녕하세요 여보소 입니다</p>
-				<p className="text-gray-700">안녕하세요 여보소 입니다</p>
-				<button>버튼</button>
+		<div className="flex flex-col min-h-screen bg-gray-100">
+			<div className="flex flex-col flex-grow container mx-auto">
+				<Bottom />
 			</div>
+			
 		</div>
 	);
 }
