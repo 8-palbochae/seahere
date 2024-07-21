f36550319e89cb99b003d34da266c12ffc206348
Jeong gi seok <wjdrltjr5@naver.com>
2024-07-21 10:04:28 +0900

    Design : user-info-component에 비밀번호변경아이템 추가

--- frontend/src/components/setting/info/UserInfoList.jsx
+++ frontend/src/components/setting/info/UserInfoList.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import UserInfoItem from "./UserInfoItem";
 import UserInfoAddressItem from "./UserInfoAddressItem";
+import UserInfoPasswordItem from "./UserInfoPasswordItem";
 
 const UserInfoList = () => {
 	return (
 		<div className="flex flex-col gap-2">
 			<UserInfoItem type={"직원명"} value={"홍길동"} />
 			<UserInfoItem type={"이메일"} value={"iwantgohome@naver.com"} />
+			<UserInfoPasswordItem type={"비밀번호 변경"} />
 			<UserInfoItem type={"회사명"} value={"스파로스"} />
 			<UserInfoItem type={"사업자번호"} value={"112-00-11122"} />
 			<UserInfoAddressItem
