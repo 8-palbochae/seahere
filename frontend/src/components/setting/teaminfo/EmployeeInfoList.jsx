f6d26a9f3abfa05d579f2c728775ae126960446a
Jeong gi seok <wjdrltjr5@naver.com>
2024-07-21 20:30:20 +0900

    Design : team info design, employee remove modal

--- frontend/src/components/setting/teaminfo/EmployeeInfoList.jsx
+++ frontend/src/components/setting/teaminfo/EmployeeInfoList.jsx
@@ -1,17 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import EmployeeInfoItem from "./EmployeeInfoItem";
+import Modal from "antd/es/modal/Modal";
 
 const EmployeeList = () => {
+	const [isModalOpen, setIsModalOpen] = useState(false);
+	const showModal = () => {
+		setIsModalOpen(true);
+	};
+	const handleCancel = () => {
+		setIsModalOpen(false);
+	};
 	return (
 		<div className="flex flex-col gap-3 items-center p-3">
-			<EmployeeInfoItem />
-			<EmployeeInfoItem />
-			<EmployeeInfoItem />
-			<EmployeeInfoItem />
-			<EmployeeInfoItem />
-			<EmployeeInfoItem />
-			<EmployeeInfoItem />
-			<EmployeeInfoItem />
+			<Modal
+				title="멤버 삭제"
+				footer={null}
+				open={isModalOpen}
+				onCancel={handleCancel}
+			>
+				<div className="flex flex-col gap-3">
+					<hr />
+					<button className="text-red-600 bg-blue-300 p-2 rounded-[20px]">
+						{"멤버 삭제"}
+					</button>
+				</div>
+			</Modal>
+			<div onClick={showModal}>
+				<EmployeeInfoItem />
+			</div>
+			<div onClick={showModal}>
+				<EmployeeInfoItem />
+			</div>
+			<div onClick={showModal}>
+				<EmployeeInfoItem />
+			</div>
+			<div onClick={showModal}>
+				<EmployeeInfoItem />
+			</div>
+			<div onClick={showModal}>
+				<EmployeeInfoItem />
+			</div>
+			<div onClick={showModal}>
+				<EmployeeInfoItem />
+			</div>
+			<div onClick={showModal}>
+				<EmployeeInfoItem />
+			</div>
+			<div onClick={showModal}>
+				<EmployeeInfoItem />
+			</div>
+			<div onClick={showModal}>
+				<EmployeeInfoItem />
+			</div>
+			<div onClick={showModal}>
+				<EmployeeInfoItem />
+			</div>
 		</div>
 	);
 };
