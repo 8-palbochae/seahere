0fc3ee4c360dea5ba5f18d39fe8b377b7a310077
장호영 <jang643@naver.com>
2024-07-22 00:09:36 +0900

    Design : 출고 리스트 출고 내역 방식 수정

--- frontend/src/components/outgoing/OutgoingListComponent.jsx
+++ frontend/src/components/outgoing/OutgoingListComponent.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import OutgoingListHeader from './OutgoingListHeader';
 import OutgoingListItem from './OutgoingListItem';
 
 const OutgoingListComponent = () => {
+    const items = Array.from({ length: 10 }, (_, index) => index + 1);
+
     return (
         // <div className="w-full px-4">
         <div className="w-full flex flex-col items-center px-4 gap-2">
-            <OutgoingListHeader />
-            <OutgoingListItem />
+            {items.map(item => (
+                <OutgoingListItem key={item} /> // 각 InventoryItem에 고유한 key를 추가합니다
+            ))}
         </div>
     );
 };
