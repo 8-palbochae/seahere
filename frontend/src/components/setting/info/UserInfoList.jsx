e579b33ce59d6b7198df60eba4967c1684a445df
Jeong gi seok <wjdrltjr5@naver.com>
2024-07-21 09:58:16 +0900

    Design : user-info-component에 주소 표시 아이템 추가

--- frontend/src/components/setting/info/UserInfoList.jsx
+++ frontend/src/components/setting/info/UserInfoList.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import UserInfoItem from "./UserInfoItem";
+import UserInfoAddressItem from "./UserInfoAddressItem";
 
 const UserInfoList = () => {
 	return (
@@ -8,6 +9,12 @@ const UserInfoList = () => {
 			<UserInfoItem type={"이메일"} value={"iwantgohome@naver.com"} />
 			<UserInfoItem type={"회사명"} value={"스파로스"} />
 			<UserInfoItem type={"사업자번호"} value={"112-00-11122"} />
+			<UserInfoAddressItem
+				type={"사업자 주소"}
+				address={"부산 해운대구 APEC로 17 센텀리더스마크"}
+				address2={"4층"}
+				postNumber={"48060"}
+			/>
 		</div>
 	);
 };
