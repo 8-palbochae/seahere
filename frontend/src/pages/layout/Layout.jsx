717840763ce0da613bf919538890a7d5fd67f1de
정기석(Gi Seok Jeong) <48271492+wjdrltjr5@users.noreply.github.com>
2024-07-21 09:09:35 +0900

    Feature/setting component design add logout (#10)
    * Design : main setting logout add and header bottom fixed
    
    * Design : main setting logout add and header bottom fixed

--- frontend/src/pages/layout/Layout.jsx
+++ frontend/src/pages/layout/Layout.jsx
@@ -5,15 +5,17 @@ import Bottom from "../../components/common/bottom/Bottom";
 
 const Layout = () => {
 	return (
-		<div className=" flex flex-col gap-8 w-full max-w-4xl bg-white shadow-md min-h-screen justify-between">
-			<div>
-				<Header />
-			</div>
-			<div className="flex-grow overflow-y-auto">
-				<Outlet className="h-full" />
-			</div>
-			<div>
-				<Bottom />
+		<div className="flex flex-col items-center w-full bg-gray-100 min-h-screen">
+			<div className="flex flex-col w-full max-w-4xl bg-white shadow-md min-h-screen">
+				<div className="fixed top-0  w-full max-w-4xl z-10">
+					<Header />
+				</div>
+				<div className="flex-grow overflow-y-auto mt-16 mb-16">
+					<Outlet />
+				</div>
+				<div className="fixed bottom-0  w-full max-w-4xl z-10">
+					<Bottom />
+				</div>
 			</div>
 		</div>
 	);
