c33285a907f896cb073a00b6eff0b70b90035e50
kimdodo97 <eh4536@naver.com>
2024-07-22 16:41:33 +0900

    Design : 도움 요청 목록 장바구니 조회 버튼 추가

--- frontend/src/components/trade/TradeInventory.jsx
+++ frontend/src/components/trade/TradeInventory.jsx
@@ -13,17 +13,37 @@ const TradeInventory = () => {
 
     return (
         <>
-            <div 
-                className='w-full cursor-pointer' 
-                onClick={handleOpenModal}
-            >
-                <TradeInventoryContent />
-            </div>
-            <Modal isOpen={isModalOpen} onClose={handleCloseModal}>
-                <div className='p-4 w-full'>
+            <div className='relative min-h-screen pb-16 w-full'>
+                {/* Main Content Area */}
+                <div 
+                    className='w-full cursor-pointer'
+                    onClick={handleOpenModal}
+                >
+                    <TradeInventoryContent />
+                    <TradeInventoryContent />
+                    <TradeInventoryContent />
+                    <TradeInventoryContent />
+                    <TradeInventoryContent />
+                    <TradeInventoryContent />
+                    <TradeInventoryContent />
+                    <TradeInventoryContent />
                     <TradeInventoryContent />
                 </div>
-            </Modal>
+                
+                {/* Modal Component */}
+                <Modal isOpen={isModalOpen} onClose={handleCloseModal}>
+                    <div className='p-4 w-full'>
+                        <TradeInventoryContent />
+                    </div>
+                </Modal>
+            </div>
+
+            {/* Button Positioned at the Bottom of the Viewport */}
+            <div className='fixed bottom-16 left-0 w-full px-4 pb-3 z-50 bg-white outline-none'>
+                <button className='bg-blue-600 w-full text-white p-3 font-bold rounded-md'>
+                    {`도움 요청 목록 (0)건`}
+                </button>
+            </div>
         </>
     );
 };
