8d7042118253038a46ad483bbbfe4302c4f59f15
정기석(Gi Seok Jeong) <48271492+wjdrltjr5@users.noreply.github.com>
2024-07-23 22:47:15 +0900

    Fix/import refactoring (#50)
    * Fix : 주석 삭제 cart -> carts
    
    * Refactor : app.js setting import refacotoring
    
    * Refactor : pages, login_signup import refactoring
    
    * Refactor : history, trade import refactor
    
    * Fix : BrokerInventory navigate cart -> carts

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,37 +1,43 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import LoginChoice from "./pages/LoginChoice";
-import Layout from "./pages/layout/Layout";
-import Setting from "./pages/Setting";
-import Main from "./pages/Main";
-import History from "./components/history/main/page/History.jsx";
-import InventoryView from "./pages/InventoryView";
-import Income from "./pages/Income";
-import SignUp from "./pages/SignUp.jsx";
-import SignUpChoice from "./components/login_signup/pagecomponent/SignUpChoice.jsx";
-import SignUpBroker from "./components/login_signup/pagecomponent/SignUpBroker.jsx";
-import SignUpBrokerChoice from "./components/login_signup/pagecomponent/SignUpBrokerChoice.jsx";
-import SignUpInfo from "./components/login_signup/pagecomponent/SignUpInfo.jsx";
-import MainSetting from "./components/setting/main/MainSetting";
-import OutgoingList from "./components/history/outgoing/page/OutgoingList.jsx";
-import QrInfo from "./components/setting/qrinfo/QrInfo";
-import UserInfoSetting from "./components/setting/userinfo/UserInfoSetting";
-import PasswordChange from "./components/setting/userinfo/password_change/PasswordChange";
-import TeamInfo from "./components/setting/teaminfo/TeamInfo";
-import InventorySetting from "./components/setting/inventoryinfo/InventorySetting";
-
-import OutgoingReqList from "./components/history/outgoing/page/OutgoingReqList.jsx";
-import Alarm from "./components/setting/alarm/Alarm";
-import IncomingList from "./components/history/incoming/page/IncomingList.jsx";
-import AdjustList from "./components/history/adjust/page/AdjustList.jsx";
+import {
+	LoginChoice,
+	Layout,
+	Setting,
+	Main,
+	InventoryView,
+	Income,
+	SignUp,
+	HistoryOutlet,
+	TradeView,
+	Cart,
+} from "./pages";
+import {
+	MainSetting,
+	UserInfoSetting,
+	QrInfo,
+	PasswordChange,
+	TeamInfo,
+	InventorySetting,
+	Alarm,
+} from "./components/setting";
+import {
+	SignUpChoice,
+	SignUpBroker,
+	SignUpBrokerChoice,
+	SignUpInfo,
+} from "./components/login_signup";
+import {
+	History,
+	OutgoingList,
+	IncomingList,
+	OutgoingReqList,
+	AdjustList,
+} from "./components/history";
+import { TradeMain, TradeBrokerMain } from "./components/trade";
 import AlarmHistory from "./components/common/header/AlarmHistory";
-import Cart from "./pages/Cart";
 import BrokerMain from "./components/main/broker/BrokerMain";
-import TradeMain from "./components/trade/TradeMain";
-import TradeView from "./pages/TradeView";
-import TradeBrokerMain from "./components/trade/TradeBrokerMain";
-import HistoryOutlet from "./pages/HistoryOutlet.jsx";
 
 function App() {
 	return (
@@ -61,29 +67,27 @@ function App() {
 						<Route path="/main" element={<Main />}>
 							<Route path="" element={<BrokerMain />} />
 						</Route>
-
 						<Route path="/histories" element={<HistoryOutlet />}>
-						<Route path="" element={<History />} />
-						<Route
-							path="outgoings"
-							element={<OutgoingList />}
-						/>
-						<Route
-							path="incomingList"
-							element={<IncomingList />}
-						/>
-						<Route
-							path="outgoings/requests"
-							element={<OutgoingReqList />}
-						/>
-						<Route path="adjusts" element={<AdjustList />} />
-					</Route>
-						
+							<Route path="" element={<History />} />
+							<Route
+								path="outgoings"
+								element={<OutgoingList />}
+							/>
+							<Route
+								path="incomingList"
+								element={<IncomingList />}
+							/>
+							<Route
+								path="outgoings/requests"
+								element={<OutgoingReqList />}
+							/>
+							<Route path="adjusts" element={<AdjustList />} />
+						</Route>
 						<Route
 							path="/inventories"
 							element={<InventoryView />}
 						/>
-						{/* inventories */}
+
 						<Route path="/trades" element={<TradeView />}>
 							<Route path="" element={<TradeMain />} />
 							<Route
@@ -91,11 +95,8 @@ function App() {
 								element={<TradeBrokerMain />}
 							/>
 						</Route>
-						<Route path="/cart" element={<Cart />} /> {/* carts */}
+						<Route path="/carts" element={<Cart />} />
 						<Route path="/incoming" element={<Income />} />
-						
-						
-						
 						<Route
 							path="/alarm-history"
 							element={<AlarmHistory />}
@@ -111,8 +112,6 @@ function App() {
 							element={<SignUpBrokerChoice />}
 						/>
 					</Route>
-					
-					
 				</Routes>
 			</BrowserRouter>
 		</div>
