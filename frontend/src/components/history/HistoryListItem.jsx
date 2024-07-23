494f84fb82a9f80f340f02731d85047e235c701a
DOYOUNG KIM <82021950+kimdodo97@users.noreply.github.com>
2024-07-23 14:17:59 +0900

    Design : 히스토리 컴포넌트 디자인 변경 (#37)

--- frontend/src/components/history/HistoryListItem.jsx
+++ frontend/src/components/history/HistoryListItem.jsx
@@ -1,50 +1,47 @@
 import React from 'react';
-import {ReactComponent as Outgoing} from "../../assets/history/outgoing-icon.svg"
-import {ReactComponent as Incoming} from "../../assets/history/incoming-icon.svg"
-import {ReactComponent as Adjust} from "../../assets/history/adjust-icon.svg"
+import { ReactComponent as Outgoing } from "../../assets/history/outgoing-icon.svg";
+import { ReactComponent as Incoming } from "../../assets/history/incoming-icon.svg";
+import { ReactComponent as Adjust } from "../../assets/history/adjust-icon.svg";
 import useHistoryHandler from '../../hooks/History/historyHandler';
+import dayjs from 'dayjs';
 
-
-const HistoryListItem = ({type}) => {
-
-    const {handleNavigation} = useHistoryHandler();
+const HistoryListItem = ({ type }) => {
+    const { handleNavigation } = useHistoryHandler();
     
-  // 아이콘 매핑 객체
-  const iconMap = {
-    출고: <Outgoing className="w-7 h-7" style={{ marginLeft: '20px', marginTop:'2px'}} />,
-    입고: <Incoming className="w-7 h-7" style={{ marginLeft: '20px', marginTop:'2px' }} />,
-    조정: <Adjust className="w-7 h-7" style={{ marginLeft: '20px',marginTop:'2px' }} />,
-};
+    // 아이콘 매핑 객체
+    const iconMap = {
+        출고: <Outgoing className="w-full h-full object-cover fill-red-600" style={{ marginLeft: '20px', marginTop: '2px' }} />,
+        입고: <Incoming className="w-full h-full object-cover fill-blue-600" style={{ marginLeft: '20px', marginTop: '2px' }} />,
+        조정: <Adjust className="w-full h-full object-cover fill-green-600" style={{ marginLeft: '20px', marginTop: '2px' }} />,
+    };
 
-// 선택된 아이콘
-const icon = iconMap[type];
+    const typeColor = {
+        출고 : 'text-red-600',
+        입고 : 'text-blue-600',
+        조정 : 'text-green-600',
+    }
+    // 선택된 아이콘
+    const icon = iconMap[type];
+
     return (
-        
-            <div className="flex flex-col justify-center w-full">
-                <div className="relative w-[376px] h-[159px] bg-blue-300 rounded-[20px] border-2 border-gray-300" onClick={() => handleNavigation(type)}>
-                <div className="flex flex-col">
-                  <div className="absolute w-[196px] h-8 top-[7px] left-2 bg-white rounded-[20px]">
-                  {icon}
-                          <div className="w-[109px] top-[7px] left-[42px] text-base whitespace-nowrap absolute text-black text-center tracking-[0] leading-[normal]">
-                              {type}
-                          </div>
-                   </div>
-                      
-                    <div className="absolute w-[360px] h-[99px] top-12 left-2 bg-[100%_100%]">
-                          <div className="absolute w-[311px] h-[53px] top-[22px] left-[25px] bg-[#d9d9d9] rounded-[10px]" />
-                          <div className="absolute w-[311px] h-[53px] top-[22px] left-[25px] bg-[#d9d9d9] rounded-[10px]" />
-                          <div className="absolute w-[1px] h-[39px] top-[30px] left-1/2 transform -translate-x-1/2 bg-gray-500"></div>
-                          <div className="absolute w-[107px] top-[35px] left-[46px] text-xl text-black text-center tracking-[0] leading-[normal]">
-                              5 품목
-                          </div>
-                          <div className="w-[107px] top-[35px] left-[195px] text-xl absolute text-black text-center tracking-[0] leading-[normal]">
-                              16 개
-                          </div>
-                      </div>
-                  </div>
+        <div className='flex flex-col text-green justify-center items-center w-full gap-3 mb-1 
+        shadow-lg rounded-lg p-4 bg-white border-t-2 border-gray-200' onClick={() => handleNavigation(type)}>
+            <div className='w-11/12 mx-3 text-left text-gray-400'>
+                {dayjs().format('YYYY-MM-DD')}
+            </div>
+            <div className='flex w-full justify-between'>
+                <div className='flex gap-10 items-center'>
+                    <div className='w-12 h-12'>
+                        {icon}
+                    </div>
+                    <span className={`font-bold text-lg ${typeColor[type]}`}>{type}</span>
                 </div>
-            
+                <div className='flex justify-around items-center gap-5'>
+                    <div className='text-lg text-gray-600 font-semibold'>5품목</div>
+                    <div className='text-lg text-gray-600 font-semibold'>16개</div>
                 </div>
+            </div>
+        </div>
     );
 };
 
