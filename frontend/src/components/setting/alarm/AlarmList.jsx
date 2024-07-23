2764754c80693031780868e7ca4b1ba9a37daaab
Jeong gi seok <wjdrltjr5@naver.com>
2024-07-23 15:54:57 +0900

    Fix : setting-design-qr

--- frontend/src/components/setting/alarm/AlarmList.jsx
+++ frontend/src/components/setting/alarm/AlarmList.jsx
@@ -5,7 +5,7 @@ const AlarmList = () => {
 	return (
 		<div className="flex flex-col gap-2 w-full ">
 			<div>{"품목"}</div>
-			<div className="flex flex-col gap-2 overflow-auto max-h-[500px] m-3">
+			<div className="flex flex-col gap-2 overflow-auto max-h-[50vh]  m-3">
 				<AlarmListItem />
 				<AlarmListItem />
 				<AlarmListItem />
