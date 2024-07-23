c273834af90b5e6599281d6136b4a8a4f28be946
정기석(Gi Seok Jeong) <48271492+wjdrltjr5@users.noreply.github.com>
2024-07-23 10:10:31 +0900

    Feature/main component (#33)
    * init : frontend common css
    
    * Init : 프론트엔드 폰트 설정
    
    * Design : header, main 기본 위치
    
    * Design : Footer 디자인 추가
    
    * Design : hader
    
    * Design : header, bottom 기본 틀
    
    * Design : Broker Main Component Design
    
    ---------
    
    Co-authored-by: kimdodo97 <eh4536@naver.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -25,9 +25,10 @@ import Alarm from "./components/setting/alarm/Alarm";
 import IncomingList from "./pages/IncomingList";
 import AdjustList from "./pages/AdjustList";
 import AlarmHistory from "./components/common/header/AlarmHistory";
-import Trade from './pages/Trade';
-import BrokerDetailPage from './pages/BrokerDetailPage';
-import Cart from './pages/Cart';
+import Trade from "./pages/Trade";
+import BrokerDetailPage from "./pages/BrokerDetailPage";
+import Cart from "./pages/Cart";
+import BrokerMain from "./components/main/broker/BrokerMain";
 
 function App() {
 	return (
@@ -54,13 +55,18 @@ function App() {
 							/>
 							<Route path="alarm" element={<Alarm />} />
 						</Route>
-						<Route path="/main" element={<Main />} />
+						<Route path="/main" element={<Main />}>
+							<Route path="" element={<BrokerMain />} />
+						</Route>
 						<Route path="/history" element={<History />} />
 						<Route path="/inventory" element={<InventoryView />} />
 
 						<Route path="/trade" element={<Trade />} />
-						<Route path="/broker/:id" element={<BrokerDetailPage />} />
-						<Route path="/cart" element={<Cart/>}/>
+						<Route
+							path="/broker/:id"
+							element={<BrokerDetailPage />}
+						/>
+						<Route path="/cart" element={<Cart />} />
 
 						<Route path="/incoming" element={<Income />} />
 						<Route path="/outgoing" />
