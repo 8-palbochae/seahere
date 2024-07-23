a4605982aa1e5bfd545792c1579ee356a9a512bb
정기석(Gi Seok Jeong) <48271492+wjdrltjr5@users.noreply.github.com>
2024-07-23 13:20:32 +0900

    Fix : userinfo item design change (#36)

--- frontend/src/components/setting/userinfo/UserInfoAddressItem.jsx
+++ frontend/src/components/setting/userinfo/UserInfoAddressItem.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const UserInfoAddressItem = ({ type, address, address2, postNumber }) => {
 	return (
-		<div className="flex flex-col gap-2 bg-blue-300 rounded-[20px] p-3">
+		<div className="flex flex-col gap-2 bg-white border-b-2 rounded-[20px] p-3">
 			<div className="items-start">{type}</div>
 			<div className="flex flex-col items-center p-3">
 				<div className="justify-self-center">{address}</div>
