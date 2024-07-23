73db8526b1e0705acc7616b22ba129e6c594282b
정기석(Gi Seok Jeong) <48271492+wjdrltjr5@users.noreply.github.com>
2024-07-23 13:09:39 +0900

    Fix/setting url router (#35)
    setting url 복수형으로 변경

--- frontend/src/components/setting/userinfo/UserInfoPasswordItem.jsx
+++ frontend/src/components/setting/userinfo/UserInfoPasswordItem.jsx
@@ -5,7 +5,7 @@ import { useNavigate } from "react-router-dom";
 const UserInfoPasswordItem = ({ type }) => {
 	const navigate = useNavigate();
 	const onClick = () => {
-		navigate("/setting/password-change");
+		navigate("/setting/password");
 	};
 	return (
 		<div
