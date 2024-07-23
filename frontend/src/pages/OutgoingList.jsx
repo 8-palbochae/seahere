98a28a65d39870b0cda28678b985c6c14af06456
안채원 <160825485+woneveryday@users.noreply.github.com>
2024-07-23 16:35:45 +0900

    Fix : 출고 리스트 디자인 변경 (#46)

--- frontend/src/pages/OutgoingList.jsx
+++ frontend/src/pages/OutgoingList.jsx
@@ -2,13 +2,12 @@ import React from 'react';
 import NoQRSearchInput from '../components/common/NoQRSearchInput';
 import OutgoingListHeader from '../components/outgoing/OutgoingListHeader';
 import OutgoingListComponent from '../components/outgoing/OutgoingListComponent';
-
+import SearchInputFilter from '../components/common/SearchInputFilter'
 const OutgoingList = () => {
     return (
         <div>
             <div>
-                <NoQRSearchInput />
-                <OutgoingListHeader />
+                <SearchInputFilter />
                 <OutgoingListComponent />
             </div>
         </div>
