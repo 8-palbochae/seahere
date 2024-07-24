83f641d657ce2e3827a4465c35336cc654690e0e
장호영 <53540017+jang643@users.noreply.github.com>
2024-07-24 09:32:33 +0900

    Fix : 헤더 디자인 종 아이콘 위치 수정 및 헤더 기준 선 굵기 및 색상 수정 (#51)

--- frontend/src/components/common/header/Header.jsx
+++ frontend/src/components/common/header/Header.jsx
@@ -9,7 +9,7 @@ export const Header = () => {
 	};
 	return (
 		<div>
-			<div className="flex justify-between h-16 bg-white border-b-2 border-black">
+			<div className="flex justify-between h-16 bg-white border-b border-D9D9D9">
 				<div className="" style={{ width: 24, height: 24 }}></div>
 				<div className="flex justify-items-center items-center text-center font-bold text-lg">
 					헤더입니다
@@ -19,7 +19,7 @@ export const Header = () => {
 						onClick={() => onClick("/alarm-history")}
 						src={headerIcon.bellIcon}
 						alt="종 모양 아이콘"
-						className="w-6 h-6 mb-1"
+						className="w-6 h-6 mr-3"
 					/>
 				</div>
 			</div>
