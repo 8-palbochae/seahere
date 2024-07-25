c7bf2babb7e1a2ce1f763880aa68b2bb154c8e6b
Jeong gi seok <wjdrltjr5@naver.com>
2024-07-25 10:15:21 +0900

    Fix : security config csrf disable, cors config add

--- backend/src/main/java/com/seahere/backend/common/config/SecurityConfig.java
+++ backend/src/main/java/com/seahere/backend/common/config/SecurityConfig.java
@@ -4,12 +4,26 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class SecurityConfig {
+public class SecurityConfig implements WebMvcConfigurer {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http.authorizeHttpRequests().anyRequest().permitAll().and().build();
+        return http.csrf().disable()
+                .cors()
+                .and()
+                .authorizeHttpRequests().anyRequest().permitAll()
+                .and().build();
+    }
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
