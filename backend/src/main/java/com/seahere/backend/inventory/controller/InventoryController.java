238a8ca808be11d65feb4f99bb52f27865689a0c
woneveryady <woneveryday@naver.com>
2024-07-26 13:37:16 +0900

    Feat : 검색창에 광어 입력 후 입고 등록 페이지 이동 기능 구현(with 자동 입력)

--- backend/src/main/java/com/seahere/backend/inventory/controller/InventoryController.java
+++ backend/src/main/java/com/seahere/backend/inventory/controller/InventoryController.java
@@ -1,23 +1,23 @@
-package com.seahere.backend.inventory.controller;
-
-import com.seahere.backend.inventory.domain.Product;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-import com.seahere.backend.common.exception.SeaHereException;
-import com.seahere.backend.common.response.ErrorResponse;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
-import com.seahere.backend.inventory.service.InventoryService;
-
-import java.util.List;
-
-@RestController
-public class InventoryController {
-    InventoryService inventoryService;
-
-    @GetMapping
-    public List<Product> findAll(Long companyId){
-        List<Product> products = inventoryService.findAll(companyId);
-        return products;
-    }
-}
+//package com.seahere.backend.inventory.controller;
+//
+//import com.seahere.backend.inventory.domain.Product;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//import com.seahere.backend.common.exception.SeaHereException;
+//import com.seahere.backend.common.response.ErrorResponse;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.http.ResponseEntity;
+//import com.seahere.backend.inventory.service.InventoryService;
+//
+//import java.util.List;
+//
+//@RestController
+//public class InventoryController {
+//    InventoryService inventoryService;
+//
+//    @GetMapping
+//    public List<Product> findAll(Long companyId){
+//        List<Product> products = inventoryService.findAll(companyId);
+//        return products;
+//    }
+//}
